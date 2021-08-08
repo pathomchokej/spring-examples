139bc0c37beb4ed81f297d779b7e526c879adb0d
pathomchokej <86038423+pathomchokej@users.noreply.github.com>
2021-08-08 21:35:44 +0700

    21-24 create bean factory

--- spring-demo-one/src/main/java/com/luv2code/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/HelloSpringApp.java
@@ -2,8 +2,17 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.File;
+
 public class HelloSpringApp {
     public static void main(String[] args){
+        // applicationContext.xml must be keep in resources folder.
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ICoach theCoach = context.getBean("baseballCoach", ICoach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        context.close();
     }
 }
---
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/HockeyCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class HockeyCoach implements ICoach{
+    @Override
+    public String getDailyWorkout() {
+        return "Play Hockey";
+    }
+}
