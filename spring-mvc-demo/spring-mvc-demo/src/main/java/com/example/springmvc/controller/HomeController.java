51242c863758d71ee9a8d96d286980560137cabc
Tummeng <pathomchoke.j@gmail.com>
2021-08-15 22:10:16 +0700

    120-125 - Replace HttpServletRequest with @RequestParam - If we use @RequestParam when request param is null. that page can't working (return 400 bad request) - If we use HttpServletRequest we can handle by check null or empty.

--- spring-mvc-demo/spring-mvc-demo/src/main/java/com/example/springmvc/controller/HomeController.java
+++ spring-mvc-demo/spring-mvc-demo/src/main/java/com/example/springmvc/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,10 +26,18 @@ public class HomeController {
         return "test-process";
     }
 
-    @RequestMapping("/processForm")
+    //@RequestMapping("/processForm")
     public String processForm(HttpServletRequest request, Model model){
         String name = request.getParameter("studentName");
-        String newMessage = "Yo! " + ((null != name && !name.isEmpty())? name.toUpperCase() : "Anonymous");
+        String newMessage = "Yo!! " + ((null != name && !name.isEmpty())? name.toUpperCase() : "Anonymous");
+        model.addAttribute("message", newMessage);
+
+        return "test-process2";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@RequestParam("studentName") String name, Model model){
+        String newMessage = "Yo!!! " + ((null != name && !name.isEmpty())? name.toUpperCase() : "Anonymous");
         model.addAttribute("message", newMessage);
 
         return "test-process2";
